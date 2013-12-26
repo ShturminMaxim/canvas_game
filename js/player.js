efb8d6691a38304a43d7bd5ef8a4b7c9fed69a7e
Shturmin <koyotka@tahoo.com>
2013-12-26 17:42:03 +0200

    add char image

--- js/player.js
+++ js/player.js
@@ -1,6 +1,22 @@
 define('player', ['mediator'], function (mediator) {
 	"use strict";
-	mediator.subscribe('game_start', function (ctx) {
-		console.log('Player is ready!!!');
-	});
+    var image = new Image();
+    var size = 50;
+    var x = 0;
+    var y = 0;
+    var posX = 10;
+    var posY = 10;
+
+    image.src = "./img/char.png";
+/*	mediator.subscribe('game_start', function (ctx) {
+
+*//*        image.onload = function(){
+            ctx.drawImage(image, 0, 0, size, size, 10, 10, size, size);
+        };*//*
+	});*/
+    return {
+        redraw : function(){
+            ctx.drawImage(image, x, y, size, size, posX, posY, size, size);
+        }
+    }
 });

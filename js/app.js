a7027655ce7b905b22d2cd077b511aa89beccc88
Oleg Kirienko <oleg.v.kirienko@gmail.com>
2013-12-26 17:56:11 +0200

    redraw event

--- js/app.js
+++ js/app.js
@@ -3,15 +3,14 @@ require(['mediator', 'event', 'stage', 'player'], function (mediator, event) {
 	var canvas = document.getElementById('game-canvas'),
 		ctx = canvas.getContext('2d');
 
-	event.bind('load', window, function () {
-		// get coordinates of the mouse click
-		event.bind('click', canvas, function (e) {
-			var mouseX = e.clientX,
-				mouseY = e.clientY,
-				elDim = canvas.getBoundingClientRect();
 
-			mediator.publish('move_to', [{x: (mouseX - elDim.left), y: (mouseY - elDim.top)}]);
-		});
+	// get coordinates of the mouse click
+	event.bind('click', canvas, function (e) {
+		var mouseX = e.clientX,
+			mouseY = e.clientY,
+			elDim = canvas.getBoundingClientRect();
+
+		mediator.publish('move_to', [{x: (mouseX - elDim.left), y: (mouseY - elDim.top)}]);
 	});
 
 	// start the game

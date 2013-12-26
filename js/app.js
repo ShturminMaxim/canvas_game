e1d94a9b4b699a0997317c78a4de0ca13af9f938
Oleg Kirienko <oleg.v.kirienko@gmail.com>
2013-12-26 17:44:02 +0200

    redraw

--- js/app.js
+++ js/app.js
@@ -1,12 +1,25 @@
-require(['mediator', 'event', 'stage', 'player'], function (mediator, event) {
+require(['mediator', 'event', 'stage', 'player'], function (mediator, event, stage, player) {
     "use strict";
 	var canvas = document.getElementById('game-canvas'),
 		ctx = canvas.getContext('2d');
 
-	event.bind('click', canvas, function (e) {
-		var offsetX = e.clientX,
-			offsetY = e.clientY;
-		mediator.publish('move_to', [{x: offsetX, y: offsetY}]);
+	event.bind('load', window, function () {
+		// get coordinates of the mouse click
+		event.bind('click', canvas, function (e) {
+			var mouseX = e.clientX,
+				mouseY = e.clientY,
+				elDim = canvas.getBoundingClientRect();
+
+			mediator.publish('move_to', [{x: (mouseX - elDim.left), y: (mouseY - elDim.top)}]);
+		});
 	});
+
+	// start the game
 	mediator.publish('game_start', [ctx]);
+
+	// loop for redrawing objects on the stage
+	setInterval(function () {
+		stage.redraw();
+		player.redraw();
+	}, 15);
 });

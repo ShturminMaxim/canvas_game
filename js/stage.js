45c6ecb1b3017cd73d6e5db3ca1385135d55d6aa
Alexandr Dzyuban <sanogtxultraracer123@gmail.com>
2013-12-27 15:54:18 +0200

    Add canvas styles

--- js/stage.js
+++ js/stage.js
@@ -8,15 +8,25 @@ define('stage', ['mediator'], function (mediator) {
 		clickedX = 0,
 		clickedY = 0,
 		curX = 0,
-		curY = 0;
+		curY = 0,
+		stopX = 0,
+		stopY = 0,
+		canvasW,
+		canvasH,
+		gameBgW,
+		gameBgH;
 
 	mediator.subscribe('game_start', function (canvasCtx, canvas) {
 		canvasNode = canvas;
 		gameBg.onload = function() {
+			gameBgW = this.width;
+			gameBgH = this.height;
 			curX = canvasNode.width / 2 - gameBg.width / 2;
 			curY = canvasNode.height / 2 - gameBg.height / 2;
 		};
 		gameBg.src = bgPath;
+		canvasW = canvasNode.width / 2;
+		canvasH = canvasNode.height / 2;
 	});
 
 	mediator.subscribe('do_animation', function(canvasPos) {
@@ -27,11 +37,21 @@ define('stage', ['mediator'], function (mediator) {
 	});
 
 	mediator.subscribe('redraw', function(canvasCtx) {
-		canvasCtx.drawImage(
-			gameBg,
-			curX,
-			curY
-		);
+		if(curX < canvasW && curX > -(gameBgW - canvasW) && curY < canvasH && curY > -(gameBgH - canvasH)) {
+			canvasCtx.drawImage(
+				gameBg,
+				curX,
+				curY
+			);
+			stopX = curX;
+			stopY = curY;
+		} else {
+			canvasCtx.drawImage(
+				gameBg,
+				stopX,
+				stopY
+			);
+		}
 	});
 
 	return {

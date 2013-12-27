6af907809dbfb4c31c8a0417b9f9ce9dcf6ec37e
Shturmin <koyotka@tahoo.com>
2013-12-27 17:13:37 +0200

    add bear movement. Bear now chasing the player.

--- js/enemies.js
+++ js/enemies.js
@@ -1,16 +1,16 @@
 define('enemies', ['mediator', 'stage'], function (mediator, stage) {
 	"use strict";
 	var posX = 10,
+        posY = 10,
 		clickedX,
 		clickedY,
-		posY = 10,
 		sizeX = 60,
 		sizeY = 80,
 		x = 0,
 		y = 0,
 		image = new Image(),
 		timer,
-		delay = 150;
+		delay = 60;
 
 	image.src = './img/bear.png';
 
@@ -19,11 +19,26 @@ define('enemies', ['mediator', 'stage'], function (mediator, stage) {
 			mediator.subscribe('redraw', function (ctx) {
 				ctx.drawImage(image, x, y, sizeX, sizeY, posX, posY, sizeX, sizeY);
 			});
+            timer = setInterval(function () {
+                var canvasX = stage.curX(),
+                    canvasY = stage.curY(),
 
-			timer = setInterval(function () {
-				posX--;
-				posY++;
-			}, delay);
+                    mx = canvasX - (stage.curX()-posX+canvas.width /2),
+                    my = (stage.curY()-posY+canvas.width /2) - canvasY,
+                    vectorLength = Math.sqrt(mx * mx + my * my),
+                    alphaRad = Math.atan2(my, mx),
+                    step = 5,
+                    stepX = step * Math.cos(alphaRad),
+                    stepY = step * Math.sin(alphaRad);
+
+                if (vectorLength>5) {
+                    posX -= stepX;
+                    posY += stepY;
+                } else {
+                    clearInterval(timer);
+                    alert('You are Dead!');
+                }
+            }, delay);
 		};
 	});
 

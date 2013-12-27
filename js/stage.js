d0b57cf78fb90fc8aba9fc8cfafa0f3214c05ab6
Alexandr Dzyuban <sanogtxultraracer123@gmail.com>
2013-12-27 13:27:02 +0200

    Add canvas styles

--- js/stage.js
+++ js/stage.js
@@ -2,38 +2,19 @@ define('stage', ['mediator'], function (mediator) {
 
 	"use strict";
 
-	var canvasNode = document.getElementById('game-canvas'),
-		gameBg,
+	var canvasNode,
+		gameBg = new Image(),
 		bgPath = 'img/game-field.jpg',
-		bgGamePos = {
-			posX: 0,
-			posY: 0,
-			sizeX: 1280,
-			sizeY: 800
-		},
-		bgClipPos = {
-			posX: 480,
-			posY: 160,
-			sizeX: 320,
-			sizeY: 480
-		},
-		startX = 0,
-		startY = 0,
-		clickedX,
-		clickedY,
-		curX,
-		curY;
+		clickedX = 0,
+		clickedY = 0,
+		curX = 0,
+		curY = 0;
 
-	canvasNode.width = 320;
-	canvasNode.height = 480;
-	gameBg = new Image();
-
-	mediator.subscribe('game_start', function (canvasCtx) {
+	mediator.subscribe('game_start', function (canvasCtx, canvas) {
 		gameBg.onload = function() {
-			curX = startX = canvasNode.width / 2 - gameBg.width / 2;
-			curY = startY = canvasNode.height / 2 - gameBg.height / 2;
-			console.log(curX);
-			console.log(curY);
+			canvasNode = canvas;
+			curX = canvasNode.width / 2 - gameBg.width / 2;
+			curY = canvasNode.height / 2 - gameBg.height / 2;
 		};
 		gameBg.src = bgPath;
 	});
@@ -42,8 +23,7 @@ define('stage', ['mediator'], function (mediator) {
 		clickedX = canvasPos.stepX;
 		clickedY = canvasPos.stepY;
 		curX += (-clickedX);
-		curY += (clickedY);
-		// change position
+		curY += clickedY;
 	});
 
 	mediator.subscribe('redraw', function(canvasCtx) {
@@ -53,20 +33,4 @@ define('stage', ['mediator'], function (mediator) {
 			curY
 		);
 	});
-
-//	return {
-//		redraw: function() {
-//			canvasCtx.drawImage(
-//				gameBg,
-//				bgClipPos.posX,
-//				bgClipPos.posY,
-//				bgClipPos.sizeX,
-//				bgClipPos.sizeY,
-//				bgGamePos.posX,
-//				bgGamePos.posY,
-//				bgGamePos.sizeX,
-//				bgGamePos.sizeY
-//			);
-//		}
-//	}
 });
